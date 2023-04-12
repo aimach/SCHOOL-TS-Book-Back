4ddd5ad8f3e5cac4aab1af9368a99c874c2de4ee
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-04-12 16:27:46 +0200

    remove depencies for upload file

--- src/resolvers/WildersResolvers.ts
+++ src/resolvers/WildersResolvers.ts
@@ -114,4 +114,18 @@ export class WildersResolvers {
       return null;
     }
   }
+
+  // add an avatar
+  @Mutation(() => Boolean)
+  async addProfilePicture(
+    @Arg("picture", () => GraphQLUpload)
+    { createReadStream, filename }: Upload
+  ): Promise<boolean> {
+    return new Promise(async (resolve, reject) =>
+      createReadStream()
+        .pipe(createWriteStream(__dirname + `/../../../images/${filename}`))
+        .on("finish", () => resolve(true))
+        .on("error", () => reject(false))
+    );
+  }
 }
